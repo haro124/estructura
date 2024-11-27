5558d5950e672709324af47e6eb2143277af4341
haro124 <haroldcarreno1@gmail.com>
2024-11-27 11:58:04 -0500

    cambiosss

---
+++ src/main/java/ec/edu/espoch/macas/Macas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.macas;
+
+/**
+ *
+ * @author Dylan Romero
+ */
+public class Macas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
