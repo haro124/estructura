3921a3dbc0f5157f44efd65778931e0a8fd4acc5
haro124 <haroldcarreno1@gmail.com>
2024-11-27 13:57:15 -0500

    uwu

--- src/main/java/ec/edu/espoch/macas/Macas.java
+++ src/main/java/ec/edu/espoch/macas/Macas.java
@@ -13,5 +13,6 @@ public class Macas {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("bienvenidos a macas ");
+        System.out.println("uwu");
     }
 }
